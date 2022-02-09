180937cbbeda4710f52c5195c64f66e412983b18
abdourahman <abdourahmanneymar3533@gmail.com>
2022-02-09 17:43:56 +0300

    dernier engistrement

---
+++ script.js
@@ -0,0 +1,54 @@
+// document.getElementById('demo').innerHTML="bonjour et bienvenue sur mon site web";
+// document.write("Hello!  "); 
+// window.alert("attention !");
+
+// let x =2;
+// let y =6;
+
+// console.log('x est :',x);
+// console.log('y est :',y);
+
+// exo1: faire un programme qui affiche la multiplication?
+let x =2;
+let a =1*2;
+let  b = 2*2;
+let c = 3*2;
+let d = 4*2;
+let e = 5*2;
+let f = 6*2;
+let g = 7*2;
+let h = 8*2;
+let i = 9*2;
+let k = 10*2;
+
+document.write(a);
+document.write(b);
+document.write(c);
+document.write(d);
+document.write(e);
+document.write(f);
+document.write(g);
+document.write(h);
+document.write(i);
+document.write(k);
+
+// fair un programme qui permet de determine les noms de vos parent une fois qu on sais 
+
+let pere =" abdi";
+let mere =" halima";
+
+let verification = pere=="abdi" || mere=="halimo";
+console.log(verification?"il a reconnu les deux parents ":"il n'a pas reconnu"); 
+let verfication=mere==name;
+document.write(verfication?"le nom de mere est reconnu":"le nom de mere ne pas reconnu");
+//faire un programmer qui permet determiner 
+let a=45;
+let d=45;
+let m;
+while (i<10){
+  m=1*5;
+}
+console.log(m);
+
+
+
